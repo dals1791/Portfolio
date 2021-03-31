27447f544b68eb3f61d2202589ba373bedc5e02b
Sean Daly <daly.se91@gmail.com>
2021-03-31 13:05:36 -0500

    stlyed Card layout an added medai queries

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,8 +1,18 @@
 import styles from '../styles/projectCard.module.css'
+import Image from 'next/image'
 
 const ProjectCard = ()=>{
+    const background= 'https://i.imgur.com/aNz6wUl.jpg?1'
     return (
-        <div className={styles.cardBody}>
+        <div className={styles.cardContainer}>
+            <div className={styles.imgContainer} 
+            style={{
+                backgroundImage:`url(${background})`
+            }}>
+                <div className={styles.cardText}>
+                    <p>lorum fnjjfnejnfjnfjndjfnjdnfjdnfjndf</p>
+                </div>
+            </div>
 
         </div>
     )
