81777a0cb7545d3e5db627669ef14abcd268659d
Sean Daly <daly.se91@gmail.com>
2021-04-08 16:24:08 -0600

    bug troubleshoot during deploy on Vercel

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,9 +1,6 @@
-import Link from "next/link";
-import Image from "next/image";
+
 import Head from "next/head";
 import Layout from "../components/layout/layout";
-import Carousel from '../components/carousel/carousel'
-
 const AboutMe = () => {
   return (
     <>
